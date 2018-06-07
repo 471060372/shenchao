39ba57dbf9b1d6c7e3c56294fd466f61f9fa3964
国民运力 <guominyunli@iMac-3.local>
2018-06-07 18:51:19 +0800

    123

---
+++ eeeee/ggViewController.h
@@ -0,0 +1,13 @@
+//
+//  ggViewController.h
+//  eeeee
+//
+//  Created by 国民运力 on 2018/5/23.
+//  Copyright © 2018年 国民运力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ggViewController : UIViewController
+
+@end
---
+++ eeeee/ggViewController.m
@@ -0,0 +1,38 @@
+//
+//  ggViewController.m
+//  eeeee
+//
+//  Created by 国民运力 on 2018/5/23.
+//  Copyright © 2018年 国民运力. All rights reserved.
+//
+
+#import "ggViewController.h"
+
+@interface ggViewController ()
+
+@end
+
+@implementation ggViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
